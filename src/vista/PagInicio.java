1ea5a24b70dc06b4e2eaaae8d794885b38d19e5e
“Marcos1dam” <juanmarcos156@hotmail.com>
2025-03-24 10:39:21 +0100

    pestaña numero 3, fondo, en la clase pagInicio

--- src/vista/PagInicio.java
+++ src/vista/PagInicio.java
@@ -43,7 +43,8 @@ public class PagInicio extends JFrame implements ActionListener {
     private JTextField textApellido;
     private JTextField textEdad;
     private JTextField textCorreo;
-    private JTextField textField;
+    private JTextField textTelefono;
+    private JLabel lblNewLabel;
 
     /**
      * Launch the application.
@@ -66,7 +67,7 @@ public class PagInicio extends JFrame implements ActionListener {
         contentPane = new JPanel() {
 
            
-            private Image backgroundImage = new ImageIcon(getClass().getResource("/imagenes/fondoCode.png")).getImage();
+            private Image backgroundImage = new ImageIcon(getClass().getResource("/imagenes/CodeAndDanceBienvenido.png")).getImage();
 
 
             @Override
@@ -88,6 +89,7 @@ public class PagInicio extends JFrame implements ActionListener {
         tabbedPane.addTab("CODE AND DANCE", null, singIn, "Información de la Pestaña 1");
 
         Password = new JPanel();
+        Password.setBackground(new Color(255, 255, 255));
         Password.setLayout(null);
         tabbedPane.addTab("Sing In", null, Password, "Información de la Pestaña 2");
 
@@ -109,11 +111,13 @@ public class PagInicio extends JFrame implements ActionListener {
         btnAcceder = new JButton("Acceder");
         btnAcceder.setFont(new Font("Arial Black", Font.PLAIN, 14));
         btnAcceder.setBounds(282, 343, 102, 21);
+        btnAcceder.addActionListener(this);
         Password.add(btnAcceder);
 
         btnCancelar = new JButton("Cancelar");
         btnCancelar.setFont(new Font("Arial Black", Font.PLAIN, 14));
         btnCancelar.setBounds(472, 343, 116, 21);
+        btnCancelar.addActionListener(this);
         Password.add(btnCancelar);
 
         btnRecuperarContraseña = new JButton("Has olvidado tu contraseña?");
@@ -125,6 +129,11 @@ public class PagInicio extends JFrame implements ActionListener {
         passwordField = new JPasswordField();
         passwordField.setBounds(356, 240, 232, 19);
         Password.add(passwordField);
+        
+        lblNewLabel = new JLabel("");
+        lblNewLabel.setIcon(new ImageIcon(PagInicio.class.getResource("/imagenes/CodeAndDance.png")));
+        lblNewLabel.setBounds(179, 34, 733, 521);
+        Password.add(lblNewLabel);
 
         /*
         JPanel panel3 = new JPanel();
@@ -180,11 +189,11 @@ public class PagInicio extends JFrame implements ActionListener {
         lblTelefono.setBounds(42, 192, 78, 16);
         panel3.add(lblTelefono);
         
-        textField = new JTextField();
-        textField.setEditable(false);
-        textField.setColumns(10);
-        textField.setBounds(130, 193, 123, 19);
-        panel3.add(textField);*/
+        textTelefono = new JTextField();
+        textTelefono.setEditable(false);
+        textTelefono.setColumns(10);
+        textTelefono.setBounds(130, 193, 123, 19);
+        panel3.add(textTelefono);*/
 
         contentPane.add(tabbedPane, BorderLayout.CENTER);
 
@@ -214,80 +223,86 @@ public class PagInicio extends JFrame implements ActionListener {
         passwordField.setText("");
     }
 
-    private void comprobar() {
-        String dni = new String(passwordField.getPassword());
-        Bailarin bailarin = Principal.leerDni(dni);
+	private void comprobar() {
+	    String dni = new String(passwordField.getPassword());
+	    Bailarin bailarin = Principal.leerDni(dni);
 
-        if (bailarin != null && dni.equalsIgnoreCase(bailarin.getDni()) && textUsuario.getText().equalsIgnoreCase(bailarin.getCorreo())) {
-            JOptionPane.showMessageDialog(this, "Bienvenido, " + bailarin.getNombre(), "Acceso concedido", JOptionPane.INFORMATION_MESSAGE);
-            
-            // Agregar la tercera pestaña si no existe
-            if (!existePestana("Información de Bailarines")) {
-            	  JPanel panel3 = new JPanel();
-                  tabbedPane.addTab("Información de Bailarines", null, panel3, "Datos de los Bailarines");
-                  panel3.setLayout(null);
-                  
-                  textNombre = new JTextField();
-                  textNombre.setEditable(false);
-                  textNombre.setBounds(130, 33, 123, 19);
-                  panel3.add(textNombre);
-                  textNombre.setColumns(10);
-                  
-                  JLabel lblNombre = new JLabel("Nombre:");
-                  lblNombre.setFont(new Font("Arial Black", Font.PLAIN, 14));
-                  lblNombre.setBounds(42, 36, 78, 16);
-                  panel3.add(lblNombre);
-                  
-                  JLabel lblApellido = new JLabel("Apellido:");
-                  lblApellido.setFont(new Font("Arial Black", Font.PLAIN, 14));
-                  lblApellido.setBounds(42, 73, 78, 16);
-                  panel3.add(lblApellido);
-                  
-                  JLabel lblEdad = new JLabel("Edad:");
-                  lblEdad.setFont(new Font("Arial Black", Font.PLAIN, 14));
-                  lblEdad.setBounds(42, 111, 78, 16);
-                  panel3.add(lblEdad);
-                  
-                  JLabel lblCorreo = new JLabel("Correo:");
-                  lblCorreo.setFont(new Font("Arial Black", Font.PLAIN, 14));
-                  lblCorreo.setBounds(42, 153, 78, 16);
-                  panel3.add(lblCorreo);
-                  
-                  textApellido = new JTextField();
-                  textApellido.setEditable(false);
-                  textApellido.setColumns(10);
-                  textApellido.setBounds(130, 74, 123, 19);
-                  panel3.add(textApellido);
-                  
-                  textEdad = new JTextField();
-                  textEdad.setEditable(false);
-                  textEdad.setColumns(10);
-                  textEdad.setBounds(130, 112, 123, 19);
-                  panel3.add(textEdad);
-                  
-                  textCorreo = new JTextField();
-                  textCorreo.setEditable(false);
-                  textCorreo.setColumns(10);
-                  textCorreo.setBounds(130, 154, 267, 19);
-                  panel3.add(textCorreo);
-                  
-                  JLabel lblTelefono = new JLabel("Telefono:");
-                  lblTelefono.setFont(new Font("Arial Black", Font.PLAIN, 14));
-                  lblTelefono.setBounds(42, 192, 78, 16);
-                  panel3.add(lblTelefono);
-                  
-                  textField = new JTextField();
-                  textField.setEditable(false);
-                  textField.setColumns(10);
-                  textField.setBounds(130, 193, 123, 19);
-                  panel3.add(textField);
-            }
+	    if (bailarin != null && dni.equalsIgnoreCase(bailarin.getDni()) && textUsuario.getText().equalsIgnoreCase(bailarin.getCorreo())) {
+	        JOptionPane.showMessageDialog(this, "Bienvenido, " + bailarin.getNombre(), "Acceso concedido", JOptionPane.INFORMATION_MESSAGE);
+	        
+	        // Agregar la tercera pestaña si no existe
+	        if (!existePestana("Información de Bailarines")) {
+	        	JPanel panel3 = new JPanel();
+	            tabbedPane.addTab("Información de Bailarines", null, panel3, "Datos de los Bailarines");
+	            panel3.setLayout(null);
+	            
+	            textNombre = new JTextField();
+	            textNombre.setEditable(false);
+	            textNombre.setBounds(130, 33, 123, 19);
+	            textNombre.setText(bailarin.getNombre());
+	            panel3.add(textNombre);
+	            textNombre.setColumns(10);
+	            
+	            JLabel lblNombre = new JLabel("Nombre:");
+	            lblNombre.setFont(new Font("Arial Black", Font.PLAIN, 14));
+	            lblNombre.setBounds(42, 36, 78, 16);
+	            panel3.add(lblNombre);
+	            
+	            JLabel lblApellido = new JLabel("Apellido:");
+	            lblApellido.setFont(new Font("Arial Black", Font.PLAIN, 14));
+	            lblApellido.setBounds(42, 73, 78, 16);
+	            panel3.add(lblApellido);
+	            
+	            JLabel lblEdad = new JLabel("Edad:");
+	            lblEdad.setFont(new Font("Arial Black", Font.PLAIN, 14));
+	            lblEdad.setBounds(42, 111, 78, 16);
+	            panel3.add(lblEdad);
+	            
+	            JLabel lblCorreo = new JLabel("Correo:");
+	            lblCorreo.setFont(new Font("Arial Black", Font.PLAIN, 14));
+	            lblCorreo.setBounds(42, 153, 78, 16);
+	            panel3.add(lblCorreo);
+	            
+	            textApellido = new JTextField();
+	            textApellido.setEditable(false);
+	            textApellido.setColumns(10);
+	            textApellido.setBounds(130, 74, 123, 19);
+	            textApellido.setText(bailarin.getApellido());
+	            panel3.add(textApellido);
+	            
+	            textEdad = new JTextField();
+	            textEdad.setEditable(false);
+	            textEdad.setColumns(10);
+	            textEdad.setBounds(130, 112, 123, 19);
+	            textEdad.setText(String.valueOf(bailarin.getEdad()));
+	            panel3.add(textEdad);
+	            
+	            textCorreo = new JTextField();
+	            textCorreo.setEditable(false);
+	            textCorreo.setColumns(10);
+	            textCorreo.setBounds(130, 154, 267, 19);
+	            textCorreo.setText(bailarin.getCorreo());
+	            panel3.add(textCorreo);
+	            
+	            JLabel lblTelefono = new JLabel("Telefono:");
+	            lblTelefono.setFont(new Font("Arial Black", Font.PLAIN, 14));
+	            lblTelefono.setBounds(42, 192, 78, 16);
+	            panel3.add(lblTelefono);
+	            
+	            textTelefono = new JTextField();
+	            textTelefono.setEditable(false);
+	            textTelefono.setColumns(10);
+	            textTelefono.setBounds(130, 193, 123, 19);
+	            textTelefono.setText(String.valueOf(bailarin.getTelefono()));
+	            panel3.add(textTelefono);
 
-            tabbedPane.setSelectedIndex(2); // Cambia a la nueva pestaña
-        } else {
-            JOptionPane.showMessageDialog(this, "DNI no registrado", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
+	        }
+
+	        tabbedPane.setSelectedIndex(2); // Cambiar a la tercera pestaña
+	    } else {
+	        JOptionPane.showMessageDialog(this, "DNI o correo incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
+	    }
+	}
 
     private boolean existePestana(String titulo) {
         for (int i = 0; i < tabbedPane.getTabCount(); i++) {
